c98cb790f4f97cbc1d0cfcec7aa200371ecf14e8
Rohan Patel <patelrohan@users.noreply.github.com>
2024-10-21 11:22:06 -0400

    Initial Commit

---
+++ NetworkCall-AsynchAwait/NetworkCall_AsynchAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCall_AsynchAwaitApp.swift
+//  NetworkCall-AsynchAwait
+//
+//  Created by Rohan Patel on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCall_AsynchAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
